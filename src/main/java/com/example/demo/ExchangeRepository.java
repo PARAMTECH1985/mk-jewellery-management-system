081ca8ce7739b0b9289150cfcb04f58e24ccbc12
Nikita <raghuvanshinikita212@gmail.com>
2026-01-08 23:06:50 +0530

    Modify ExchangeRepository queries

--- src/main/java/com/example/demo/ExchangeRepository.java
+++ src/main/java/com/example/demo/ExchangeRepository.java
@@ -7,4 +7,14 @@ public interface ExchangeRepository
         extends JpaRepository<ExchangeTransaction, Long> {
 
     List<ExchangeTransaction> findByBillId(Long billId);
+    
+    // optional (agar value > 0 wale hi chahiye)
+    List<ExchangeTransaction> findByBillIdAndOldValueGreaterThan(Long billId, double value);
+
+    // saara data (sab users ka)
+    List<ExchangeTransaction> findAll();
+
+    //  agar sirf valid value wale chahiye
+    List<ExchangeTransaction> findByOldValueGreaterThan(double value);
+
 }
