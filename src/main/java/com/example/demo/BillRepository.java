3841f80eaded71c16afb3c313df15fb735ab182a
Nikita <raghuvanshinikita212@gmail.com>
2026-01-08 23:06:35 +0530

    Update BillRepository logic

--- src/main/java/com/example/demo/BillRepository.java
+++ src/main/java/com/example/demo/BillRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BillRepository extends JpaRepository<Bean_Bill, Long>{
+public interface BillRepository extends JpaRepository<Bean_Bill, Long> {
 
-	List<Bean_Bill> findByBillNo(String billNo);
+    List<Bean_Bill> findByBillNo(String billNo);
+
 }
