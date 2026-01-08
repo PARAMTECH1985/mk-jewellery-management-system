b0c67a63175879576da66beec60e95f8e463a305
Nikita <raghuvanshinikita212@gmail.com>
2026-01-08 23:06:41 +0530

    Update BillService interface

--- src/main/java/com/example/demo/BillService.java
+++ src/main/java/com/example/demo/BillService.java
@@ -3,14 +3,9 @@ package com.example.demo;
 import java.util.List;
 
 public interface BillService {
-
-	Bean_Bill saveBill(Bean_Bill bill);
-
+    Bean_Bill saveBill(Bean_Bill bill);
     List<Bean_Bill> getAllBills();
-    
-    
     void deleteById(Long id);
-
     Bean_Bill getBillById(Long id);
-
+    List<Bean_Bill> findByBillNo(String billNo);
 }
