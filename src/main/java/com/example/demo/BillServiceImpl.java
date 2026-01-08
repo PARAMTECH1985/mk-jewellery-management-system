daffc625a27dadb699147275996bb5eb419705e9
Nikita <raghuvanshinikita212@gmail.com>
2026-01-08 23:06:46 +0530

    Enhance BillServiceImpl implementation

--- src/main/java/com/example/demo/BillServiceImpl.java
+++ src/main/java/com/example/demo/BillServiceImpl.java
@@ -7,43 +7,64 @@ import org.springframework.stereotype.Service;
 @Service
 public class BillServiceImpl implements BillService {
 
-	 private final BillRepository repo;
+    private final BillRepository repo;
+    private final GlobalRateRepository rateRepo;
 
-	    public BillServiceImpl(BillRepository repo) {
-	        this.repo = repo;
-	    }
-	    
-	   
+    public BillServiceImpl(BillRepository repo, GlobalRateRepository rateRepo) {
+        this.repo = repo;
+        this.rateRepo = rateRepo;
+    }
+    
+    @Override
+    public List<Bean_Bill> findByBillNo(String billNo) {
+        return repo.findByBillNo(billNo);
+    }
 
-		@Override
-		public Bean_Bill saveBill(Bean_Bill bill) {
-			
-			
-			return repo.save(bill);
-		}
+    @Override
+    public Bean_Bill saveBill(Bean_Bill bill) {
 
-		@Override
-		public List<Bean_Bill> getAllBills() {
-			return repo.findAll();
-		}
+        GlobalRate globalRate = rateRepo.findAll()
+                                        .stream()
+                                        .findFirst()
+                                        .orElse(null);
 
-		public List<Bean_Bill> findByBillNo(String billNo) {
-		
-			return repo.findByBillNo(billNo);
-		}
-		
-		
-		
-		@Override
-		public void deleteById(Long id) {
-		    repo.deleteById(id);
-		}
+        if (globalRate != null && bill.getRate() != null) {
 
-		
-		@Override
-		public Bean_Bill getBillById(Long id) {
-		    return repo.findById(id).orElse(null);
-		}
+            List<Double> updatedRates = bill.getRate();
 
+            for (int i = 0; i < updatedRates.size(); i++) {
 
-	   }
+                if ("Gold".equalsIgnoreCase(bill.getDescription())) {
+                    updatedRates.set(i, globalRate.getGoldRate());
+                }
+                else if ("Silver".equalsIgnoreCase(bill.getDescription())) {
+                    updatedRates.set(i, globalRate.getSilverRate());
+                }
+            }
+
+            bill.setRate(updatedRates);
+        }
+
+        return repo.save(bill);
+    }
+
+    
+    
+    @Override
+    public List<Bean_Bill> getAllBills() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public Bean_Bill getBillById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+    
+    
+   
+}
